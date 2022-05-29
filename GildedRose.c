89019ded09b53db4a31ca8a5167abd23ffe90c27
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2022-05-29 19:07:28 +0400

    Refactor: automatic simplify remaining behaviours

--- GildedRose.c
+++ GildedRose.c
@@ -69,42 +69,16 @@ void update_quality_single(Item *item) {
         return;
     }
 
-    if (!is_backage_passes)
-    {
-        if (item->quality > 0)
-        {
-            item->quality = item->quality - 1;
-        }
-    }
-    else
-    {
-        if (item->quality < 50)
-        {
-            item->quality = item->quality + 1;
-
-            if (item->sellIn < 11) {
-                if (item->quality < 50) {
-                    item->quality = item->quality + 1;
-                }
-            }
-            if (item->sellIn < 6) {
-                if (item->quality < 50) {
-                    item->quality = item->quality + 1;
-                }
-            }
-        }
+    if (item->quality > 0) {
+        item->quality = item->quality - 1;
     }
 
     item->sellIn = item->sellIn - 1;
 
     if (item->sellIn < 0)
     {
-        if (!is_backage_passes) {
-            if (item->quality > 0) {
-                item->quality = item->quality - 1;
-            }
-        } else {
-            item->quality = item->quality - item->quality;
+        if (item->quality > 0) {
+            item->quality = item->quality - 1;
         }
     }
 }
