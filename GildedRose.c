8272d84132548c6db8eda28c2fa72ceb1bb9b4d5
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2022-05-29 18:48:11 +0400

    Refactor: extract explaining method

--- GildedRose.c
+++ GildedRose.c
@@ -18,22 +18,66 @@ sprint_item(char* buffer, Item* item)
     sprintf(buffer, "%s, %d, %d", item->name, item->sellIn, item->quality);
 }
 
-void 
-update_quality(Item items[], int size) 
-{
-    int i;
-    
-    for (i = 0; i < size; i++)
+void update_quality_single(Item *item) {
+    if (strcmp(item->name, "Aged Brie") && strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert"))
     {
-        Item *item = &items[i];
-        if (strcmp(item->name, "Aged Brie") && strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert"))
+        if (item->quality > 0)
         {
-            if (item->quality > 0)
+            if (strcmp(item->name, "Sulfuras, Hand of Ragnaros"))
             {
-                if (strcmp(item->name, "Sulfuras, Hand of Ragnaros"))
+                item->quality = item->quality - 1;
+            }
+        }
+    }
+    else
+    {
+        if (item->quality < 50)
+        {
+            item->quality = item->quality + 1;
+
+            if (!strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert"))
+            {
+                if (item->sellIn < 11)
                 {
-                    item->quality = item->quality - 1;
+                    if (item->quality < 50)
+                    {
+                        item->quality = item->quality + 1;
+                    }
                 }
+
+                if (item->sellIn < 6)
+                {
+                    if (item->quality < 50)
+                    {
+                        item->quality = item->quality + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    if (strcmp(item->name, "Sulfuras, Hand of Ragnaros"))
+    {
+        item->sellIn = item->sellIn - 1;
+    }
+
+    if (item->sellIn < 0)
+    {
+        if (strcmp(item->name, "Aged Brie"))
+        {
+            if (strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert"))
+            {
+                if (item->quality > 0)
+                {
+                    if (strcmp(item->name, "Sulfuras, Hand of Ragnaros"))
+                    {
+                        item->quality = item->quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                item->quality = item->quality - item->quality;
             }
         }
         else
@@ -41,59 +85,19 @@ update_quality(Item items[], int size)
             if (item->quality < 50)
             {
                 item->quality = item->quality + 1;
-
-                if (!strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert"))
-                {
-                    if (item->sellIn < 11)
-                    {
-                        if (item->quality < 50)
-                        {
-                            item->quality = item->quality + 1;
-                        }
-                    }
-
-                    if (item->sellIn < 6)
-                    {
-                        if (item->quality < 50)
-                        {
-                            item->quality = item->quality + 1;
-                        }
-                    }
-                }
             }
         }
+    }
+}
 
-        if (strcmp(item->name, "Sulfuras, Hand of Ragnaros"))
-        {
-            item->sellIn = item->sellIn - 1;
-        }
+void
+update_quality(Item items[], int size)
+{
+    int i;
 
-        if (item->sellIn < 0)
-        {
-            if (strcmp(item->name, "Aged Brie"))
-            {
-                if (strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert"))
-                {
-                    if (item->quality > 0)
-                    {
-                        if (strcmp(item->name, "Sulfuras, Hand of Ragnaros"))
-                        {
-                            item->quality = item->quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    item->quality = item->quality - item->quality;
-                }
-            }
-            else
-            {
-                if (item->quality < 50)
-                {
-                    item->quality = item->quality + 1;
-                }
-            }
-        }
+    for (i = 0; i < size; i++)
+    {
+        Item *item = &items[i];
+        update_quality_single(item);
     }
 }
