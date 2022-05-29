574abb8f0d5e0d97a468f33825f8f15ee34f37fb
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2022-05-29 18:47:17 +0400

    Refactor: extract variable before extracting a method

--- GildedRose.c
+++ GildedRose.c
@@ -25,72 +25,73 @@ update_quality(Item items[], int size)
     
     for (i = 0; i < size; i++)
     {
-        if (strcmp(items[i].name, "Aged Brie") && strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
+        Item *item = &items[i];
+        if (strcmp(item->name, "Aged Brie") && strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert"))
         {
-            if (items[i].quality > 0)
+            if (item->quality > 0)
             {
-                if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
+                if (strcmp(item->name, "Sulfuras, Hand of Ragnaros"))
                 {
-                    items[i].quality = items[i].quality - 1;
+                    item->quality = item->quality - 1;
                 }
             }
         }
         else
         {
-            if (items[i].quality < 50)
+            if (item->quality < 50)
             {
-                items[i].quality = items[i].quality + 1;
+                item->quality = item->quality + 1;
 
-                if (!strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
+                if (!strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert"))
                 {
-                    if (items[i].sellIn < 11)
+                    if (item->sellIn < 11)
                     {
-                        if (items[i].quality < 50)
+                        if (item->quality < 50)
                         {
-                            items[i].quality = items[i].quality + 1;
+                            item->quality = item->quality + 1;
                         }
                     }
 
-                    if (items[i].sellIn < 6)
+                    if (item->sellIn < 6)
                     {
-                        if (items[i].quality < 50)
+                        if (item->quality < 50)
                         {
-                            items[i].quality = items[i].quality + 1;
+                            item->quality = item->quality + 1;
                         }
                     }
                 }
             }
         }
 
-        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
+        if (strcmp(item->name, "Sulfuras, Hand of Ragnaros"))
         {
-            items[i].sellIn = items[i].sellIn - 1;
+            item->sellIn = item->sellIn - 1;
         }
 
-        if (items[i].sellIn < 0)
+        if (item->sellIn < 0)
         {
-            if (strcmp(items[i].name, "Aged Brie"))
+            if (strcmp(item->name, "Aged Brie"))
             {
-                if (strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
+                if (strcmp(item->name, "Backstage passes to a TAFKAL80ETC concert"))
                 {
-                    if (items[i].quality > 0)
+                    if (item->quality > 0)
                     {
-                        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
+                        if (strcmp(item->name, "Sulfuras, Hand of Ragnaros"))
                         {
-                            items[i].quality = items[i].quality - 1;
+                            item->quality = item->quality - 1;
                         }
                     }
                 }
                 else
                 {
-                    items[i].quality = items[i].quality - items[i].quality;
+                    item->quality = item->quality - item->quality;
                 }
             }
             else
             {
-                if (items[i].quality < 50)
+                if (item->quality < 50)
                 {
-                    items[i].quality = items[i].quality + 1;
+                    item->quality = item->quality + 1;
                 }
             }
         }
